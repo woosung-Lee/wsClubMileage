c9e9a2aebbc4ebd787c7e437e58b657d6aab0d33
woosung-Lee <64631529+woosung-Lee@users.noreply.github.com>
2022-06-20 16:46:11 +0900

    Initial commit

---
+++ src/main/java/me/ws/wsclubmileage/WsClubMileageApplication.java
@@ -0,0 +1,13 @@
+package me.ws.wsclubmileage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsClubMileageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WsClubMileageApplication.class, args);
+    }
+
+}
